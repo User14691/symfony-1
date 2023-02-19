37556f564d34da2b0581e7325d56aed9ec7ce669
Popeye <bastien.bourgeat1@gmail.com>
2023-02-19 18:44:29 +0100

    fin

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * Page d'accueil
+     * 
+     * @return  Response
+     */
+    public function index(): Response
+    {
+        // Entity Manager de Symfony
+        $em         = $this->getDoctrine()->getManager();
+        // On récupère tous les articles disponibles en base de données
+        $articles   = $em->getRepository(Article::class)->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'articles'  => $articles
+        ]);
+    }
+}
